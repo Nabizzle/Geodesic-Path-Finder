b8d0a21d5755c5f167389b3e7cd3f650275e4e9e
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2023-08-23 12:23:34 -0400

    refactor: Converts pandas to polars
    polars runs faster

--- Code/Surface Area From Drawing.py
+++ Code/Surface Area From Drawing.py
@@ -3,14 +3,14 @@
 from scipy.spatial import KDTree
 import numpy as np
 import matplotlib.path as mpltPath
-import pandas as pd
+import polars as pl
 import cv2
 import pyvista as pv
 from hydra import initialize, compose
 
 
 # Define Function to Find UV vertex indicies
-def find_uv_index_kdtree(border_points: pd.DataFrame, image_x_size: int,
+def find_uv_index_kdtree(border_points: pl.DataFrame, image_x_size: int,
                          image_y_size: int) -> int:
     '''
     Converts one location drawing border pixel to a UV value
@@ -29,7 +29,7 @@ def find_uv_index_kdtree(border_points: pd.DataFrame, image_x_size: int,
     indicies: int
     The row numbers of the closest uv to the 2D border point list
     '''
-    data_array = border_points.copy().to_numpy(dtype=float)
+    data_array = border_points.to_numpy().astype(np.float16)
     data_array[:, 0] = data_array[:, 0] / image_x_size
     data_array[:, 1] = 1 - (data_array[:, 1] / image_y_size)
     _, indicies = KDTree(uv_array).query(data_array)
@@ -39,8 +39,10 @@ def find_uv_index_kdtree(border_points: pd.DataFrame, image_x_size: int,
 with initialize(version_base=None, config_path="config"):
     cfg = compose(config_name='config.yaml')
 # Load in data
-data = pd.read_csv(f"{cfg.data_path}ExampleBoundaryTable.csv")
-data.head()
+data = (
+    pl.read_csv(f"{cfg.data_path}ExampleBoundaryTable.csv")
+    .with_columns(pl.col("*").cast(pl.Int16))
+    )
 
 # Load in Male Right Arm Mesh Data
 imported_data =\
@@ -52,11 +54,12 @@ mesh_faces = imported_data["mesh_faces"]
 uv_array = imported_data["uv_array"]
 
 # import the face data
-face_data = pd.DataFrame(imported_data["face_data"],
-                         columns=["vertex", "uv", "normal"])
+face_data = pl.DataFrame(imported_data["face_data"],
+                         schema={"vertex": pl.UInt32, "uv": pl.UInt32,
+                                 "normal": pl.UInt32})
 
 # Load in the Male Right Arm and Find the image dimensions
-img = cv2.imread('../Media/right arm.png', 1)
+img = cv2.imread(cfg.drawing_templates.image, 1)
 image_x_size = img.shape[1]
 image_y_size = img.shape[0]
 
@@ -95,16 +98,13 @@ indicies_of_sorted_indicies = np.argsort(combined_uv_array)
 sorted_indicies = combined_uv_array[indicies_of_sorted_indicies]
 
 # Make a look up table of one for one UV to Vertex Points
-face_data_reduced = face_data[["vertex", "uv"]].drop_duplicates()
-face_data_reduced = face_data_reduced.sort_values(by=['uv'])
+face_data_reduced = face_data[["vertex", "uv"]].unique()
+face_data_reduced = face_data_reduced.sort(by=['uv'])
 
 # Find all Verticies at Once and Reorganize the Array Based on the Sort
-
-face_data_reduced[face_data_reduced['uv'].isin(combined_uv_array)]
-
 sorted_vertex_ids =\
-    face_data_reduced[face_data_reduced['uv']
-                      .isin(combined_uv_array)]["vertex"].to_numpy()
+    face_data_reduced.filter(face_data_reduced['uv']
+                             .is_in(combined_uv_array))["vertex"].to_numpy()
 
 vertex_ids = np.empty((len(combined_uv_array))).astype(int)
 
