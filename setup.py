9d2422220f4b47f8df0c1ceb3701b39df772e6a3
Nabeel <16357000+Nabizzle@users.noreply.github.com>
2024-02-13 13:40:47 -0500

    docs: Removes the requirement for pandas

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Geodesic Path Finder",
-    version="1.0.0",
+    version="1.1.0",
     author="Nabeel Chowdhury",
     author_email="nabeel.chowdhury@case.edu",
     py_modules=["Geodesic Path Finder"],
@@ -17,12 +17,12 @@ setup(
     keywords=["Geodesic", "Location Drawing", "Heat Method"],
     download_url="https://github.com/Nabizzle/Geodesic-Path-Finder",
     install_requires=["customtkinter==5.1.2", "opencv-python==4.7.0.72",
-                      "numpy==1.24.2", "pandas==1.5.3", "polyscope==1.3.1",
+                      "numpy==1.24.2", "polyscope==1.3.1",
                       "potpourri3d==0.0.8", "pynput==1.7.6",
                       "jupyterlab==4.1.0", "scipy==1.10.1",
                       "matplotlib==3.7.2", "ipympl==0.9.3", "pyvista==0.41.1",
                       "trame==3.1.0", "trame-vtk==2.5.8",
                       "trame-vuetify==2.3.1", "hydra-core==1.3.2",
-                      "polars==0.20.3", "pyarrow==14.0.2"],
+                      "polars==0.20.7", "pyarrow==14.0.2"],
     platforms="windows",
 )
